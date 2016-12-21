5091974bfc3acebc6217b90051961ec4dbe40407
icelxt <icelxt@163.com>
2016-12-21 14:54:03 +0800

    Springboot改造

---
+++ src/main/java/oa/App.java
@@ -0,0 +1,26 @@
+package oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @包名：oa
+ * @类名：App
+ * @创建人：孙伟隆
+ * @创建时间：2016年12月21日 下午2:51:38
+ * 
+ * @描述：TODO
+ *
+ * @SVN版本号：$Rev$
+ * @更新时间：$Date$
+ * @更新人：$Author$
+ * @更新描述：TODO 
+ * 
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
